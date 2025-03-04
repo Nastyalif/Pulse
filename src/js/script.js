0545c25fbc318ff21b80d9c53508c6e628c917ca
Nastyalif <miroshanastasia2007@gmail.com>
2025-03-05 01:31:18 +0200

    change main title

--- src/js/script.js
+++ src/js/script.js
@@ -35,4 +35,59 @@ $(document).ready(function(){
 
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
+
+      // Modal
+
+  $('[data-modal=consultation]').on('click', function(){ /* fadeOut() - красивое/анимированное скрытие элементов на странице */
+      $('.overlay, #consultation').fadeIn('slow');
   });
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');/* сначала закрываються все неинтересуюшийся элементы и говорит, что эта часть будет fadeOut и выполняться оно будет медленно*/
+  });
+
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click',function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); /* внутри модального окна есть класс. Во внутрь помешяеться текст. eq - позволяют брать элементы попорядку  */
+      $('.overlay, #order').fadeIn('slow');
+    })
+  });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+
+  $('input[name=phone]').mask("+380 (99) 999-99-99")
+
+  $('form').submit(function(e) {
+    e.precventDefault();/* отменить стандарстное поведения браузера */
+  });
+});
