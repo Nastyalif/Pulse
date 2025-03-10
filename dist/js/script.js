aab2640909e031be5d4fc71ecff7e1100760fcf4
Nastyalif <miroshanastasia2007@gmail.com>
2025-03-10 23:47:44 +0200

    connect with dist

---
+++ dist/js/script.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    // dots: true, /* кружочки, которые можно подставить и застилизовать - будут в навигации по доному слайду */
+    speed: 1200, /* как быстро один слайд переключался на другой */
+    // adaptiveHeight: true, /* будут картинки разной высоты */
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    responsive: [ /* [] - обозначает масив даних */
+      {
+        breakpoint: 992, /* на каком промежутке устанавливают правила */
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+    });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this) /* .closest - команда, которая говорит, что нужно найти ближайший элемент */
+        .index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+          $(item).each(function(i) {
+              $(this).on('click', function(e) {
+                  e.preventDefault();
+                  $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                  $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+              })
+          });
+      };
+
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+
+      // Modal
+
+  $('[data-modal=consultation]').on('click', function(){ /* fadeOut() - красивое/анимированное скрытие элементов на странице */
+      $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');/* сначала закрываються все неинтересуюшийся элементы и говорит, что эта часть будет fadeOut и выполняться оно будет медленно*/
+  });
+
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click',function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); /* внутри модального окна есть класс. Во внутрь помешяеться текст. eq - позволяют брать элементы попорядку  */
+      $('.overlay, #order').fadeIn('slow');
+    })
+  });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  };
+
+  
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+  $('form').submit(function (e) {
+    e.preventDefault();
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function () {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  // Smooth scroll and pageup
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href=#up]").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
+
+  new WOW().init();
+});
+  
+
+
+    
+    
+    
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,18 @@ const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');/* все лишние пробелы и т.д. будут убраны */
+
+gulp.task('server', function() {
+    browserSync ({
+        server: {
+            baseDir:"dist"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
 
 gulp.task("fonts", function () {
     return gulp
@@ -20,13 +32,13 @@ function styles() {
         .pipe(gulp.dest('./src/css')) // Сохраняем обычный style.css
         .pipe(cleanCSS({ compatibility: 'ie8' })) // Минифицируем
         .pipe(rename({ suffix: '.min' })) // Добавляем .min к имени файла
-        .pipe(gulp.dest('./src/css')) // Сохраняем минифицированный style.min.css
+        .pipe(gulp.dest('dist/css')) // Сохраняем минифицированный style.min.css
         .pipe(browserSync.stream()); // Обновляем браузер
 }
 
 // Таск для обработки font.css
 function fontStyles() {
-    return gulp.src("src/css/font.css")  // Указываем правильный путь к font.css
+    return gulp.src("src/sass/libs/_font.scss")  // Указываем правильный путь к font.css
         .pipe(postcss([autoprefixer()]))    // Добавляем автопрефиксы
         .pipe(cleanCSS())                   // Минифицируем CSS
         .pipe(rename({ suffix: '.min' }))    // Добавляем .min к имени файла
@@ -41,49 +53,55 @@ function watch() {
             baseDir: "src" // Где лежит index.html
         }
     });
-    gulp.watch("src/sass/**/*.+(scss|sass)", styles); // Следим за SCSS и запускаем styles /**/ <---говорит, что мы следим за всеми файлами такого расшырения, который есть в sass, но и за всеми папками и файлами внутри
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", styles); // Следим за SCSS и запускаем styles /**/ <---говорит, что мы следим за всеми файлами такого расшырения, который есть в sass, но и за всеми папками и файлами внутри
     gulp.watch("src/css/font.css", fontStyles); // Следим за изменениями в font.css и запускаем fontStyles
-    gulp.watch("src/*.html").on('change', browserSync.reload); // Обновляем HTML при изменении
+    gulp.watch("src/*.html").on('change', gulp.parallel('html')); // Обновляем HTML при изменении
 }
 
-// Экспортируем задачи
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+
+});
+
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js") /* ** - что бы можна было брать с абсолютно любой папки */
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*") /* ** - что бы можна было брать с абсолютно любой папки */
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*") /* ** - что бы можна было брать с абсолютно любой папки */
+        .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('mailer', function () {
+    return gulp.src("src/mailer/**/*") /* ** - что бы можна было брать с абсолютно любой папки */
+        .pipe(gulp.dest("dist/mailer"));
+});
+
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*") /* ** - что бы можна было брать с абсолютно любой папки */
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('styles', styles);
+gulp.task('watch', watch);
+gulp.task('fontStyles', fontStyles);
+
 exports.styles = styles;
 exports.fontStyles = fontStyles;
 exports.watch = watch;
-exports.default = gulp.series(styles, fontStyles, watch);
+// exports.default = gulp.series(styles, fontStyles, watch);
 
+gulp.task('default', gulp.series(
+    gulp.parallel('styles', 'scripts', 'fonts', 'icons', 'mailer', 'images'),
+    'watch', 'server'
+));
 
-
-// const gulp = require('gulp'); /* *указывает какие пакеты используются в проекте  */
-// const browserSync = require('browser-sync');
-// const sass = require('gulp-sass')(require('sass')); /* теперь можно использовать компилятор sass */
-// const cleanCSS = require('gulp-clean-css');
-// const autoprefixer = require('gulp-autoprefixer');
-// const rename = require("gulp-rename");
-
-// gulp.task('server', function () { /* настройка самого простого сервера browserSync*/
-
-//     browserSync({ 
-//         server: {
-//             baseDir: "src"
-//         }
-//     });
-
-//     gulp.watch("src/*.html").on('change', browserSync.reload);
-// });
-
-// gulp.task('styles', function () {
-//     return gulp.src("src/sass/**/*.+(scss|sass)") /* при помощи src можно выполнять различные операции над данным файлом */
-//         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError)) /* сказано какой стиль будет в итоге */
-//         .pipe(rename({ suffix: '.min', prefix: '' }))
-//         .pipe(autoprefixer())
-//         .pipe(cleanCSS({ compatibility: 'ie8' }))
-//         .pipe(gulp.dest("src/css"))
-//         .pipe(browserSync.stream());
-// });
-
-// gulp.task('watch', function () { /* gulp следит как за стелистическими файлами так и за html файлом  */
-//     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-// })
-
-// gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
